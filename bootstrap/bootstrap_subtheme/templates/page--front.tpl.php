1fd211808f5ced69c6eb14d57f1f4abcf7cf0999
Arun <arunr@acube.co>
2014-08-22 10:47:32 +0530

    added style folder to template

---
+++ bootstrap/bootstrap_subtheme/templates/page--front.tpl.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=EDGE">
+	<meta charset='UTF-8' />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=3">
+    <title>Bootstrap 101 Template</title>
+
+  </head>
+  <body>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container nav_container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Radio2 </a>
+        </div>
+        <div class="navbar-collapse collapse">
+		 <ul class="nav navbar-nav">
+			<li class="active"><a href="#">Home</a></li>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Contact Us</a></li>
+		  </ul>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
+
+    <div class="container radio2-container"><!-- Main container Strats--> 
+	
+		<div class="row"><!-- Main row Starts-->
+			<div class="col-ie-3 col-xs-3 col-sm-12 col-md-3 col-lg-3 redcolorr"><!-- col-md-3 Starts-->
+				<p>Logo</p>
+			</div><!-- col-md-3 Ends-->
+			<div class="col-ie-7 col-xs-7 col-sm-12 col-md-7 col-lg-7 greycolortop"><!-- col-md-7 Starts-->
+				<p>Timed Banner</p>
+			</div><!-- col-md-7 Ends-->
+			<div class="col-ie-2 col-xs-2 col-sm-12 col-md-2 col-lg-2 pinkcolor"><!-- col-md-2 Starts-->
+			<div class="divider_top">Span 1</div>
+			<div class="divider_bottom">Span 2</div>
+				<p>Right End Content</p>
+			</div><!-- col-md-2 Ends-->
+		</div><!-- Main row Ends -->	
+	
+		<!-- Slideshow Starts -->
+		<div class="row">
+			<div class="col-ie-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 slideshow_color"><!-- col-md-12 Starts-->
+				<p>Slideshow</p>
+			</div><!-- col-md-12 Ends-->	
+		</div>
+		<!-- Slideshow Ends -->
+	
+	
+	
+	
+		<div class="row"><!-- Main row Starts-->
+			<div class="col-ie-8 col-sm-12 col-md-8 skybluecolor"><!-- col-md-8 Starts--> 
+			
+			<!-- 8 column Stuufs -->	
+			
+			<!--Radio2 TV Starts -->
+			<div class="row"><!-- row Starts For Heading-->
+					<div class="col-ie-12 col-md-12 lime_yellow_color">
+						<h2>Radio2 TV</h2>
+					</div>
+			</div><!-- row Ends For Heading-->
+			<div class="row">
+				<div class="col-ie-6 col-sm-6 col-md-6">
+					<div class="redcolor">
+					</div>
+				</div>
+				<div class="col-ie-6 col-sm-6 col-md-6">
+					<div class="redcolors">
+					</div>
+				</div>
+			</div>	
+			<!--Radio2 TV Ends -->
+			
+
+			<!-- Tabloid Starts -->
+			<div class="row"><!-- row Starts For Heading-->
+				<div class="col-ie-12 col-sm-12 col-md-12 lightyellow">
+					<h2>Tabloid</h2>
+				</div>
+			</div><!-- row Ends For Heading-->
+			<div class="row"><!-- row Starts-->
+					<div class="col-ie-3 col-xs-12 col-sm-12 col-md-3 col-lg-3 greycolor">
+						<p>Donec sed odio dui. Cras justo odio, dapibus ac facilis</p>
+					</div>
+					<div class="col-ie-3 col-xs-12 col-sm-12 col-md-3 col-lg-3 lightgreycolor">
+						<p>Donec sed odio dui. Cras justo odio, dapibus ac facilis</p>
+					</div>
+					<div class="col-ie-3 col-xs-12 col-sm-12 col-md-3 col-lg-3 greycolors">
+						<p>Donec sed odio dui. Cras justo odio, dapibus ac facilis</p>
+					</div>
+					<div class="col-ie-3 col-xs-12 col-sm-12 col-md-3 col-lg-3 lightgreycolors">
+						<p>Donec sed odio dui. Cras justo odio, dapibus ac facilis</p>
+					</div>
+			</div>  <!-- row end-->
+			<!-- Tabloid Ends -->
+			
+			
+			<!--Hot on Radio2 Starts -->
+			<div class="row"><!-- row Starts For Heading-->
+				<div class="col-ie-12 col-md-12 lightyellowhot">
+					<h2>Hot On Radio2</h2>
+				</div>
+			</div><!-- row Ends For Heading-->
+			<div class="row"><!-- row Starts-->
+				<div class="col-ie-4 col-sm-12 col-md-4 pinkcolors"><!-- col-md-4 Starts-->
+					  <h2>4 Column</h2>
+						<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis euismod semper</p>
+				</div><!-- col-md-4 Ends-->
+				<div class="col-ie-4 col-sm-12 col-md-4 purplecolor"><!-- col-md-4 Starts-->
+					  <h2>4 Column</h2>
+						<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis euismod semper</p>
+				</div><!-- col-md-4 Ends-->
+				<div class="col-ie-4 col-sm-12 col-md-4 darkpurplecolor"><!-- col-md-4 Starts-->
+					  <h2>4 Column</h2>
+						<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis euismod semper</p>
+				</div><!-- col-md-4 Ends-->
+			</div><!-- row Ends-->
+			<!--Hot on Radio2 Ends -->
+			
+			<!-- Music Starts-->
+			<div class="row"><!-- row Starts For Heading-->
+				<div class="col-ie-12 col-sm-12 col-md-12 lime_yellow_color">
+					<h2>Music</h2>
+				</div>
+			</div><!-- row Ends For Heading-->
+			<div class="row">
+				<div class="col-ie-1 col-sm-2 col-md-1 kennyshow_number">
+					<p>01</p>
+				</div>
+				<div class="col-ie-1 col-sm-2 col-md-1 kennyshow_number">
+					<p>01</p>
+				</div>
+				<div class="col-ie-10 col-sm-8 col-md-10 kennyshow_contents">
+					<p>Kenny Show</p>
+				</div>
+				<div class="col-ie-1 col-sm-2 col-md-1 kennyshow_number">
+					<p>02</p>
+				</div>
+				<div class="col-ie-1 col-sm-2 col-md-1 kennyshow_number">
+					<p>02</p>
+				</div>
+				<div class="col-ie-10 col-sm-8 col-md-10 kennyshow_contents">
+					<p>Second Kenny Show</p>
+				</div>
+			</div>
+			<!-- Music Ends-->
+			
+			<!-- Contests Starts-->
+			<div class="row"><!-- row Starts For Heading-->
+					<div class="col-ie-12 col-sm-12 col-md-12 lime_yellow_color">
+						<h2>Contents</h2>
+					</div>
+			</div><!-- row Ends For Heading-->
+			<div class="row">
+				<div class="col-ie-3 col-sm-12 col-md-3 lightgreencolor">
+					<p>01</p>
+				</div>
+				
+				<div class="col-ie-3 col-sm-12 col-md-3 lightgreenscolor">
+					<p>01</p>
+				</div>
+				
+				<div class="col-ie-3 col-sm-12 col-md-3 darkgreencolor">
+					<p>01</p>
+				</div>
+				
+				<div class="col-ie-3 col-sm-12 col-md-3 lightgreenscolor">
+					<p>01</p>
+				</div>
+
+			</div>	
+			<!-- Contest Ends-->
+
+		
+			
+			
+			
+			
+			
+			<!-- 8 column Stuufs Ends-->
+				
+			</div><!-- col-md-8 Ends--> 
+			
+			
+			
+			
+			<!-- 4 column Start  -->			
+			
+			
+			<div class="col-ie-4 col-sm-12 col-md-4 facebook_right"><!-- col-md-4 Starts--> 
+				<p>Facebook</p>
+			</div><!-- col-md-4 Ends-->
+			 	
+			<div class="col-ie-4 col-sm-12 col-md-4 advertisement_right"><!-- col-md-4 Starts--> 
+				<p>Advertisement</p>
+			</div><!-- col-md-4 Ends-->
+			
+			<div class="col-ie-4 col-sm-12 col-md-4 photos_right"><!-- col-md-4 Starts--> 
+				<p>Photos</p>
+			</div><!-- col-md-4 Ends-->
+			
+			<div class="col-ie-4 col-sm-12 col-md-4 blog_right"><!-- col-md-4 Starts--> 
+				<p>Second Ad</p>
+			</div><!-- col-md-4 Ends-->
+			
+			<div class="col-ie-4 col-sm-12 col-md-4 weather_right"><!-- col-md-4 Starts--> 
+				<p>Weather</p>
+			</div><!-- col-md-4 Ends-->
+			
+
+			<!-- 4 column Ends  -->		
+			
+			
+			
+			
+			
+		</div>	<!-- Main row Ends-->
+		
+
+		
+	</div><!-- Main container Ends--> 	
+	
+	<!-- Sub Footer Starts -->
+		
+		
+			
+			<div class="col-ie-12 col-sm-12 col-md-12 subfooter_subone">
+				<div class="container subfooter-container"><!-- Main container Strats--> 
+					<div class="col-ie-5 col-sm-12 col-md-5 subfooter_subtwo">
+						<p>SubFooter 1</p>
+					</div>
+					<div class="col-ie-5 col-sm-12 col-md-5 subfooter_subone">
+						<p>SubFooter 2</p>
+					</div>
+					<div class="col-ie-2 col-sm-12 col-md-2 subfooter_subthree">
+						<p>SubFooter 3</p>
+					</div>
+				</div>
+			</div>	
+			
+		
+			
+	<!-- Sub Footer Ends -->
+	
+
+	<!-- Footer Starts -->
+		
+		
+			
+			<div class="col-ie-12 col-sm-12 col-md-12 mainfooter">
+				<div class="container mainfooter-container"><!-- Main container Strats--> 
+					<div class="col-ie-5 col-md-5 col-sm-12 copyright_footer">
+						<p>GN Broadcasting Copyright 2013.</p>
+					</div>
+				</div>
+			</div>	
+			
+		
+			
+	<!-- Footer Ends -->
+	
+	
+	
+
+	
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+        <script src="sites/all/themes/bootstrap/bootstrap_subtheme/js/vendor/bootstrap.min.js"></script>
+
+        <script src="sites/all/themes/bootstrap/bootstrap_subtheme/js/main.js"></script>
+		
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+	
+		
+  </body>
+</html>
