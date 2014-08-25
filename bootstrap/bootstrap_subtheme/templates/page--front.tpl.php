aebf87f91996891eccd5fb7bc7482ab7c7b3d572
Arun <arunr@acube.co>
2014-08-25 17:06:56 +0530

    implemeted new dummy data

--- bootstrap/bootstrap_subtheme/templates/page--front.tpl.php
+++ bootstrap/bootstrap_subtheme/templates/page--front.tpl.php
@@ -33,25 +33,233 @@
     <div class="container radio2-container"><!-- Main container Strats--> 
 	
 		<div class="row"><!-- Main row Starts-->
-			<div class="col-ie-3 col-xs-3 col-sm-12 col-md-3 col-lg-3 redcolorr"><!-- col-md-3 Starts-->
-				<p>Logo</p>
+			<div class="col-ie-3 col-xs-3 col-sm-12 col-md-3 col-lg-3 logo"><!-- col-md-3 Starts-->
+				
 			</div><!-- col-md-3 Ends-->
-			<div class="col-ie-7 col-xs-7 col-sm-12 col-md-7 col-lg-7 greycolortop"><!-- col-md-7 Starts-->
-				<p>Timed Banner</p>
+			<div class="col-ie-7 col-xs-7 col-sm-12 col-md-7 col-lg-7 timed_banner"><!-- col-md-7 Starts-->
+				
 			</div><!-- col-md-7 Ends-->
             
-			<div class="col-ie-2 col-xs-2 col-sm-12 col-md-2 col-lg-2 pinkcolor"><!-- col-md-2 Starts-->
-			<div class="divider_one">Span 1</div>
-			<div class="divider_two">Span 2</div>
-			<div class="divider_three">Span 3</div>
-				
+			<div class="col-ie-2 col-xs-2 col-sm-12 col-md-2 col-lg-2 listen_live"><!-- col-md-2 Starts-->
+
 			</div><!-- col-md-2 Ends-->
 		</div><!-- Main row Ends -->	
 	
 		<!-- Slideshow Starts -->
 		<div class="row">
-			<div class="col-ie-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 slideshow_color"><!-- col-md-12 Starts-->
-				<p>Slideshow</p>
+			<div class="col-ie-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 no_padding_left"><!-- col-md-12 Starts-->
+				<!-- Third Party Slideshow Begin -->
+				    <!-- Jssor Slider Begin -->
+						<!-- You can move inline styles to css file or css block. -->
+						    <script>
+
+							jQuery(document).ready(function ($) {
+								var options = {
+									$AutoPlay: true,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+									$AutoPlaySteps: 1,                                  //[Optional] Steps to go for each navigation request (this options applys only when slideshow disabled), the default value is 1
+									$AutoPlayInterval: 4000,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+									$PauseOnHover: 1,                               //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, default value is 1
+					
+									$ArrowKeyNavigation: true,   			            //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
+									$SlideDuration: 500,                                //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
+									$MinDragOffsetToSlide: 20,                          //[Optional] Minimum drag offset to trigger slide , default value is 20
+									//$SlideWidth: 600,                                 //[Optional] Width of every slide in pixels, default value is width of 'slides' container
+									//$SlideHeight: 300,                                //[Optional] Height of every slide in pixels, default value is height of 'slides' container
+									$SlideSpacing: 5, 					                //[Optional] Space between each slide in pixels, default value is 0
+									$DisplayPieces: 1,                                  //[Optional] Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
+									$ParkingPosition: 0,                                //[Optional] The offset position to park slide (this options applys only when slideshow disabled), default value is 0.
+									$UISearchMode: 1,                                   //[Optional] The way (0 parellel, 1 recursive, default value is 1) to search UI components (slides container, loading screen, navigator container, arrow navigator container, thumbnail navigator container etc).
+									$PlayOrientation: 1,                                //[Optional] Orientation to play slide (for auto play, navigation), 1 horizental, 2 vertical, 5 horizental reverse, 6 vertical reverse, default value is 1
+									$DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+					
+									$ThumbnailNavigatorOptions: {
+										$Class: $JssorThumbnailNavigator$,              //[Required] Class to create thumbnail navigator instance
+										$ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+					
+										$Loop: 2,                                       //[Optional] Enable loop(circular) of carousel or not, 0: stop, 1: loop, 2 rewind, default value is 1
+										$AutoCenter: 3,                                 //[Optional] Auto center thumbnail items in the thumbnail navigator container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 3
+										$Lanes: 1,                                      //[Optional] Specify lanes to arrange thumbnails, default value is 1
+										$SpacingX: 4,                                   //[Optional] Horizontal space between each thumbnail in pixel, default value is 0
+										$SpacingY: 4,                                   //[Optional] Vertical space between each thumbnail in pixel, default value is 0
+										$DisplayPieces: 4,                              //[Optional] Number of pieces to display, default value is 1
+										$ParkingPosition: 0,                            //[Optional] The offset position to park thumbnail
+										$Orientation: 2,                                //[Optional] Orientation to arrange thumbnails, 1 horizental, 2 vertical, default value is 1
+										$DisableDrag: false                             //[Optional] Disable drag or not, default value is false
+									}
+								};
+					
+								var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+					
+								//responsive code begin
+								//you can remove responsive code if you don't want the slider scales while window resizes
+								function ScaleSlider() {
+									var parentWidth = jssor_slider1.$Elmt.parentNode.clientWidth;
+									if (parentWidth) {
+										var sliderWidth = parentWidth;
+					
+										//keep the slider width no more than 810
+										sliderWidth = Math.min(sliderWidth, 959);
+					
+										jssor_slider1.$SetScaleWidth(sliderWidth);
+									}
+									else
+										window.setTimeout(ScaleSlider, 30);
+								}
+					
+								ScaleSlider();
+					
+								if (!navigator.userAgent.match(/(iPhone|iPod|iPad|BlackBerry|IEMobile)/)) {
+									$(window).bind('resize', ScaleSlider);
+								}
+					
+					
+								//if (navigator.userAgent.match(/(iPhone|iPod|iPad)/)) {
+								//    $(window).bind("orientationchange", ScaleSlider);
+								//}
+								//responsive code end
+							});
+						</script>
+
+						<div id="slider1_container" style="position: relative; top: 0px; left: 0px; width: 810px; height: 300px; background: #000; overflow: hidden; ">
+					
+							<!-- Loading Screen -->
+							<div u="loading" style="position: absolute; top: 0px; left: 0px;">
+								<div style="filter: alpha(opacity=70); opacity:0.7; position: absolute; display: block;
+									background-color: #fff; top: 0px; left: 0px;width: 100%;height:100%;">
+								</div>
+								<div style="position: absolute; display: block; background: url(sites/all/themes/bootstrap/bootstrap_subtheme/img/content/large/slideshow_images/loading.gif) no-repeat center center;
+									top: 0px; left: 0px;width: 100%;height:100%;">
+								</div>
+							</div>
+					
+							<!-- Slides Container -->
+							<div u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 600px; height: 300px;
+								overflow: hidden;">
+								<div>
+									<img u="image" src="sites/all/themes/bootstrap/bootstrap_subtheme/img/content/large/slideshow_images/002.jpg" />
+									<div u="thumb">
+										<img class="i" src="sites/all/themes/bootstrap/bootstrap_subtheme/img/content/large/slideshow_images/thumbs/thumb-002.jpg" /><div class="t">Banner Rotator</div>
+										<div class="c">360+ touch swipe slideshow effects</div>
+									</div>
+								</div>
+								<div>
+									<img u="image" src="sites/all/themes/bootstrap/bootstrap_subtheme/img/content/large/slideshow_images/002.jpg" />
+									<div u="thumb">
+										<img class="i" src="sites/all/themes/bootstrap/bootstrap_subtheme/img/content/large/slideshow_images/thumbs/thumb-002.jpg" /><div class="t">Image Gallery</div>
+										<div class="c">Image gallery with thumbnail navigation</div>
+									</div>
+								</div>
+					
+					
+							</div>
+							
+							<!-- ThumbnailNavigator Skin Begin -->
+							<div u="thumbnavigator" class="jssort11" style="position: absolute; width: 200px; height: 300px; left:605px; top:0px;">
+								<!-- Thumbnail Item Skin Begin -->
+								<style>
+									/* jssor slider thumbnail navigator skin 11 css */
+									/*
+									.jssort11 .p            (normal)
+									.jssort11 .p:hover      (normal mouseover)
+									.jssort11 .pav          (active)
+									.jssort11 .pav:hover    (active mouseover)
+									.jssort11 .pdn          (mousedown)
+									*/
+									.jssort11
+									{
+										font-family: Arial, Helvetica, sans-serif;
+									}
+									.jssort11 .i, .jssort11 .pav:hover .i
+									{
+										position: absolute;
+										top:3px;
+										left:3px;
+										WIDTH: 60px;
+										HEIGHT: 30px;
+										border: white 1px dashed;
+									}
+									* html .jssort11 .i
+									{
+										WIDTH /**/: 62px;
+										HEIGHT /**/: 32px;
+									}
+									.jssort11 .pav .i
+									{
+										border: white 1px solid;
+									}
+									.jssort11 .t, .jssort11 .pav:hover .t
+									{
+										position: absolute;
+										top: 3px;
+										left: 68px;
+										width:129px;
+										height: 32px;
+										line-height:32px;
+										text-align: center;
+										color:#fc9835;
+										font-size:13px;
+										font-weight:700;
+									}
+									.jssort11 .pav .t, .jssort11 .phv .t, .jssort11 .p:hover .t
+									{
+										color:#fff;
+									}
+									.jssort11 .c, .jssort11 .pav:hover .c
+									{
+										position: absolute;
+										top: 38px;
+										left: 3px;
+										width:197px;
+										height: 31px;
+										line-height:31px;
+										color:#fff;
+										font-size:11px;
+										font-weight:400;
+										overflow: hidden;
+									}
+									.jssort11 .pav .c, .jssort11 .phv .c, .jssort11 .p:hover .c
+									{
+										color:#fc9835;
+									}
+									.jssort11 .t, .jssort11 .c
+									{
+										transition: color 2s;
+										-moz-transition: color 2s;
+										-webkit-transition: color 2s;
+										-o-transition: color 2s;
+									}
+									.jssort11 .p:hover .t, .jssort11 .phv .t, .jssort11 .pav:hover .t, .jssort11 .p:hover .c, .jssort11 .phv .c, .jssort11 .pav:hover .c
+									{
+										transition: none;
+										-moz-transition: none;
+										-webkit-transition: none;
+										-o-transition: none;
+									}
+									.jssort11 .p
+									{
+										background:#181818;
+									}
+									.jssort11 .pav, .jssort11 .pdn
+									{
+										background:#462300;
+									}
+									.jssort11 .p:hover, .jssort11 .phv, .jssort11 .pav:hover
+									{
+										background:#333;
+									}
+								</style>
+								<div u="slides" style="cursor: move;">
+									<div u="prototype" class="p" style="position: absolute; width: 200px; height: 145px; top: 0; left: 0;">
+										<thumbnailtemplate style=" width: 100%; height: 100%; border: none;position:absolute; top: 0; left: 0;"></thumbnailtemplate>
+									</div>
+								</div>
+								<!-- Thumbnail Item Skin End -->
+							</div>
+							<!-- ThumbnailNavigator Skin End -->
+							<a style="display: none" href="http://www.jssor.com">slideshow</a>
+						</div>
+						<!-- Jssor Slider End -->
+				<!-- Third Party Slideshow Ends-->
 			</div><!-- col-md-12 Ends-->	
 		</div>
 		<!-- Slideshow Ends -->
@@ -73,10 +281,18 @@
 			<div class="row">
 				<div class="col-ie-6 col-xs-6 col-sm-12 col-md-6">
 					<div class="redcolor">
+						<iframe width="290" height="175" src="https://www.youtube.com/embed/ZyfLdydDxM8" frameborder="0" allowfullscreen></iframe>
+					</div>
+					<div class="video_description">
+						Radio2 TV
 					</div>
 				</div>
 				<div class="col-ie-6 col-xs-6 col-sm-12 col-md-6 vertical_seperator">
 					<div class="redcolors">
+						<iframe width="290" height="175" src="https://www.youtube.com/embed/Tsz6G-OXxBw" frameborder="0" allowfullscreen></iframe>
+					</div>
+					<div class="video_description">
+						GN TV
 					</div>
 				</div>
 			</div>	
@@ -158,57 +374,57 @@
 			
 			<!-- Music Starts-->
 			<div class="row"><!-- row Starts For Heading-->
-				<div class="col-ie-12 col-sm-12 col-md-12 lime_yellow_color">
+				<div class="col-ie-12 col-sm-12 col-xs-12 col-md-12 lime_yellow_color">
 					<h2>Music</h2>
 				</div>
 			</div><!-- row Ends For Heading-->
 			<div class="row">
-				<div class="col-ie-1 col-sm-1 col-md-1 kennyshow_number">
+				<div class="col-ie-1 col-sm-1 col-xs-1 col-md-1 kennyshow_number">
 					<p>01</p>
 				</div>
-				<div class="col-ie-1 col-sm-1 col-md-1 kennyshow_number">
+				<div class="col-ie-1 col-sm-1 col-xs-1 col-md-1 kennyshow_number">
 					<p>02</p>
 				</div>
-				<div class="col-ie-7 col-sm-7 col-md-7 kennyshow_contents">
+				<div class="col-ie-7 col-sm-7 col-xs-7 col-md-7 kennyshow_contents">
 					<p>Kenny Show</p>
 				</div>
 				
 				
 				<!-- -->
-				<div class="col-ie-1 col-sm-1 col-md-1 kennyshow_play">
+				<div class="col-ie-1 col-sm-1 col-xs-1 col-md-1 kennyshow_play">
 					<p>Play</p>
 				</div>
 				
-				<div class="col-ie-1 col-sm-1 col-md-1 kennyshow_pause">
+				<div class="col-ie-1 col-sm-1 col-xs-1 col-md-1 kennyshow_pause">
 					<p>Pause</p>
 				</div>
-				<div class="col-ie-1 col-sm-1 col-md-1 kennyshow_contents">
-				
+				<div class="col-ie-1 col-sm-1 col-xs-1 col-md-1 kennyshow_contents">
+					
 				</div>
 				
 
 				<!-- Second Content Starts-->
-				<div class="col-ie-1 col-sm-1 col-md-1 kennyshow_number">
+				<div class="col-ie-1 col-sm-1 col-xs-1 col-md-1 kennyshow_number">
 					<p>01</p>
 				</div>
-				<div class="col-ie-1 col-sm-1 col-md-1 kennyshow_number">
+				<div class="col-ie-1 col-sm-1 col-xs-1 col-md-1 kennyshow_number">
 					<p>02</p>
 				</div>
-				<div class="col-ie-7 col-sm-7 col-md-7 kennyshow_contents">
+				<div class="col-ie-7 col-sm-7 col-xs-7 col-md-7 kennyshow_contents">
 					<p>Kenny Show</p>
 				</div>
 				
 				
 				<!-- -->
-				<div class="col-ie-1 col-sm-1 col-md-1 kennyshow_play">
+				<div class="col-ie-1 col-sm-1 col-xs-1 col-md-1 kennyshow_play">
 					<p>Play</p>
 				</div>
 				
-				<div class="col-ie-1 col-sm-1 col-md-1 kennyshow_pause">
+				<div class="col-ie-1 col-sm-1 col-xs-1 col-md-1 kennyshow_pause">
 					<p>Pause</p>
 				</div>
-				<div class="col-ie-1 col-sm-1 col-md-1 kennyshow_contents">
-				
+				<div class="col-ie-1 col-sm-1 col-xs-1 col-md-1 kennyshow_contents">
+					
 				</div>                
 				<!-- Second Content Ends-->				
 			</div>
@@ -280,17 +496,47 @@
 			<!-- 4 column Start  -->			
 			
 			
-			<div class="col-ie-4 col-sm-12 col-md-4 facebook_right"><!-- col-md-4 Starts--> 
-				<p>Facebook</p>
+			<div class="col-ie-4 col-sm-12 col-md-4 facebook_right no_padding"><!-- col-md-4 Starts--> 
+				<!-- Social Tab Starts-->
+				<div id="tabContainer">
+					<div id="tabs">
+					  <ul>
+						<li id="tabHeader_1">Facebook</li>
+						<li id="tabHeader_2">Twitter</li>
+						<li id="tabHeader_3">Instagram</li>
+					  </ul>
+					</div>
+					<div id="tabscontent">
+					  <div class="tabpage" id="tabpage_1">
+				<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FRadio2UAE&amp;width=240&amp;height=395&amp;colorscheme=light&amp;show_faces=false&amp;header=false&amp;stream=true&amp;show_border=false&amp;appId=674151015978019" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:260px; height:395px;" allowTransparency="true"></iframe>
+					  </div>
+					  
+					  <div class="tabpage" id="tabpage_2">
+				<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FRadio1UAE&amp;width=240&amp;height=395&amp;colorscheme=light&amp;show_faces=false&amp;header=false&amp;stream=true&amp;show_border=false&amp;appId=674151015978019" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:260px; height:395px;" allowTransparency="true"></iframe>
+					  </div>
+					  
+					 <div class="tabpage" id="tabpage_3">
+				<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FRadio2UAE&amp;width=240&amp;height=395&amp;colorscheme=light&amp;show_faces=false&amp;header=false&amp;stream=true&amp;show_border=false&amp;appId=674151015978019" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:260px; height:395px;" allowTransparency="true"></iframe>
+					</div>
+				   </div>
+				</div>
+				<!-- Social Tab Ends-->
 			</div><!-- col-md-4 Ends-->
 			 	
-			<div class="col-ie-4 col-sm-12 col-md-4 advertisement_right"><!-- col-md-4 Starts--> 
-				<p>Advertisement</p>
+			<div class="col-ie-4 col-sm-12 col-md-4 no_padding_left"><!-- col-md-4 Starts--> 
+				<div class="advertisement_right">
+					Advertisement
+				</div>
 			</div><!-- col-md-4 Ends-->
+		
+			<div class="col-sm-12 col-md-4 photos_right_header">
+			<p>Photos</p>
+			</div>
 			
 			<div class="col-ie-4 col-sm-12 col-md-4 photos_right"><!-- col-md-4 Starts--> 
-				<p>Photos</p>
+				
 			</div><!-- col-md-4 Ends-->
+			
 			
 			<div class="col-ie-4 col-sm-12 col-md-4 blog_right"><!-- col-md-4 Starts--> 
 				<p>Second Ad</p>
@@ -360,6 +606,16 @@
         <script src="sites/all/themes/bootstrap/bootstrap_subtheme/js/vendor/bootstrap.min.js"></script>
 
         <script src="sites/all/themes/bootstrap/bootstrap_subtheme/js/main.js"></script>
+		<!-- Slideshow Scripts Starts-->
+		<script type="text/javascript" src="sites/all/themes/bootstrap/bootstrap_subtheme/js/slideshow/jssor.core.js"></script>
+		<script type="text/javascript" src="sites/all/themes/bootstrap/bootstrap_subtheme/js/slideshow/jssor.utils.js"></script>
+		<script type="text/javascript" src="sites/all/themes/bootstrap/bootstrap_subtheme/js/slideshow/js/jssor.slider.js"></script>
+		
+		<!-- Slideshow Scripts Ends-->
+		
+		<!-- Social Tabs Scripts Starts-->
+		<script type="text/javascript" src="sites/all/themes/bootstrap/bootstrap_subtheme/js/social_tabs/tabs.js"></script>
+		<!-- Social Tabs Scripts Ends-->
 		
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
